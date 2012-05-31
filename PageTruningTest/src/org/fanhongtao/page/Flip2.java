71f5ceb444c9630a1a5ce612eeb8eeafc0ebe0e3
Fan Hongtao <fanhongtao@gmail.com>
2012-06-01 07:14:03 +0800

    Add background color for Flip2

--- PageTruningTest/src/org/fanhongtao/page/Flip2.java
+++ PageTruningTest/src/org/fanhongtao/page/Flip2.java
@@ -24,23 +24,24 @@ public class Flip2 extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        TAG = "Flip1";
+        TAG = "Flip2";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flipper);
 
         detector = new GestureDetector(new OnGestureListener2());
         flipper = (ViewFlipper) this.findViewById(R.id.viewFlipper1);
-        flipper.addView(addTextView("Page 1"));
-        flipper.addView(addTextView("Page 2"));
-        flipper.addView(addTextView("Page 3"));
-        flipper.addView(addTextView("Page 4"));
-        flipper.addView(addTextView("Page 5"));
+        flipper.addView(addTextView("Page 1", R.drawable.color_blue));
+        flipper.addView(addTextView("Page 2", R.drawable.color_green));
+        flipper.addView(addTextView("Page 3", R.drawable.color_red));
+        flipper.addView(addTextView("Page 4", R.drawable.color_white));
+        flipper.addView(addTextView("Page 5", R.drawable.color_yellow));
     }
 
-    private View addTextView(String text) {
+    private View addTextView(String text, int id) {
         TextView tv = new TextView(this);
         tv.setText(text);
         tv.setGravity(1);
+        tv.setBackgroundResource(id);
         return tv;
     }
 
